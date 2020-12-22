6c83ebf214859a5f9db2af95f245c9e577935b07
Robinson <robinpaulista85@gmail.com>
2020-12-21 22:12:09 -0300

    Lista de exercícios

--- lista_ex6.cpp/exercicio6.cpp
+++ lista_ex6.cpp/exercicio6.cpp
@@ -1,2 +1,23 @@
 /* 6. Faça um programa que carregue um vetor de dez números. Calcule e mostre um vetor
 resultante ordenado de maneira crescente. */
+
+
+#include <stdio.h>
+int main(){
+    int TAMANHO_DO_VETOR = 10;
+    int vetor[TAMANHO_DO_VETOR];
+
+    for(int i = 0; i < TAMANHO_DO_VETOR; i++){
+        printf("Digite um valor: ");
+        scanf("%d", &vetor[i]);
+    }
+
+    for(int i = 0; i < TAMANHO_DO_VETOR; i++){
+        if(vetor[i]<vetor[i]== vetor[i]){
+            printf("%d \n", vetor[i]);
+        }
+    }
+
+    getchar ();
+    return 0;
+}
--- lista_ex6.cpp/exercicio7.cpp
+++ lista_ex6.cpp/exercicio7.cpp
@@ -1,2 +1,22 @@
 /* 7. Faça um programa que leia um vetor de 15 posições de números inteiros. Exibe 
 quantos e quais destes números são primos. */
+
+
+#include <stdio.h>
+int main(){
+    int TAMANHO_DO_VETOR = 15;
+    int vetor[TAMANHO_DO_VETOR];
+
+    for(int i = 0; i < TAMANHO_DO_VETOR; i++){
+        printf("Digite um valor inteiro: ");
+        scanf("%d", &vetor[i]);
+    }
+
+    for(int i = 0; i < TAMANHO_DO_VETOR; i++){
+        printf("Esses são números primos: %d", vetor[i]);
+    }
+
+    getchar ();
+    return 0;
+
+}
