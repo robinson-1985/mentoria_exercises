87588ee9f7c99aa42b23b6c74a84206830e482f4
Robinson <robinpaulista85@gmail.com>
2020-12-25 12:36:52 -0300

    Lista de exercícios

--- lista_ex6.cpp/exercicio5.cpp
+++ lista_ex6.cpp/exercicio5.cpp
@@ -1,17 +1,22 @@
 /* 5. Faça um programa que carregue 1 vetor de 6 elementos numéricos inteiros; calcule e
 imprima a quantidade de números pares e a quantidade de números impares. */
 
-
 #include <stdio.h>
+#include <math.h>
 int main(){
-    int TAMANHO_DO_VETOR = 7;
-    int vetor[TAMANHO_DO_VETOR];
+    int TAMANHO_DO_VETOR = 6;
+    int vetor[TAMANHO_DO_VETOR], soma_impares = 0, i;
 
     for(int i = 0; i < TAMANHO_DO_VETOR; i++){
-
+        printf("Declare os valores: ");
+        scanf("%d",&vetor[i]);
+            if (vetor[i] % 2 != 0){
+                soma_impares += vetor[i];
+            }
     }
+
+    printf("A soma dos números ímpares é: %d\n", soma_impares);
 
     getchar ();
     return 0;
-
 }
