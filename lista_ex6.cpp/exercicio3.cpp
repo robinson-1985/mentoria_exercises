323d7ef8c3a310093957f5800442bff2a7697676
Robinson <robinpaulista85@gmail.com>
2020-12-25 13:44:17 -0300

    Lista de exercícios

--- lista_ex6.cpp/exercicio3.cpp
+++ lista_ex6.cpp/exercicio3.cpp
@@ -1,6 +1,9 @@
 /* 3. Criar 3 vetores com 10 posições, preencha com números os 2 primeiros, a seguir,
 some os valores nas respectivas posições e armazene no 3o vetor. */
 
+
+//Refazer
+
 #include <stdio.h>
 int main(){
     int TAMANHO_DO_VETOR = 2;
--- lista_ex6.cpp/exercicio4.cpp
+++ lista_ex6.cpp/exercicio4.cpp
@@ -1,14 +1,28 @@
 /* 4. Criar 2 vetores com 8 elementos, preencher o primeiro com valores, a seguir
 preencha o 2° com os valores do primeiro em ordem inversa. */
 
+// Refazer
+
 #include <stdio.h>
 int main(){
-    int TAMANHO_DO_VETOR = 7;
+    int TAMANHO_DO_VETOR = 2;
     int vetor[TAMANHO_DO_VETOR];
 
     for(int i = 0; i < TAMANHO_DO_VETOR; i++){
-
+        printf("Elemento[%d]= ",i);
+        scanf("%d",&vetor[i]);
     }
+
+    printf("\n::: Valores originais :::\n");
+    for(int i = 0;i < TAMANHO_DO_VETOR; i++){
+        printf("%d\n",vetor[i]);
+    }
+
+    printf("\n::: Valores na ordem inversa :::\n");
+    for(int i = 4;i >= 0; i--){
+        printf("%d\n",vetor[i]);
+    }
+    
 
     getchar ();
     return 0;
