f457509cb40701d667926a14f45a85311fb25bb7
Robinson <robinpaulista85@gmail.com>
2020-12-28 19:13:20 -0300

    Lista de exercícios

--- lista_livro_python/exercicio5.11.cpp
+++ lista_livro_python/exercicio5.11.cpp
@@ -1,3 +1,17 @@
 /* 5.11  Escreva um programa que pergunte o depósito inicial e a taxa de juros de 
 uma poupança. Exiba os valores mês a mês para os primeiros meses. Escreva o total 
 ganho com juros no período. */
+
+#include<stdio.h>
+int main() {
+    float deposito_inicial, taxa_juros, total_ganho;
+    printf("Declare o valor do depósito: ");
+    scanf("%f", &deposito_inicial);
+    printf("Declare a taxa de juros da poupança: ");
+    scanf("%f", &taxa_juros);
+
+    
+
+    getchar();
+    return 0;
+}
--- lista_livro_python/exercicio5.12.cpp
+++ lista_livro_python/exercicio5.12.cpp
@@ -1,3 +1,19 @@
 /* 5.12 Altere o programa anterior de forma a perguntar também o valor depositado 
 mensalmente. Esse valor será depositado no início de cada mês, e você deve considerá-lo 
 para o cálculo de juros do mês seguinte.*/
+
+#include<stdio.h>
+int main() {
+    float deposito_inicial, taxa_juros, total_ganho, valor_mensal;
+    printf("Declare o valor do depósito: ");
+    scanf("%f", &deposito_inicial);
+    printf("Declare a taxa de juros da poupança: ");
+    scanf("%f", &taxa_juros);
+    printf("Declare o valor depositado por mês: ");
+    scanf("%f", &valor_mensal);
+
+    
+
+    getchar();
+    return 0;
+}
