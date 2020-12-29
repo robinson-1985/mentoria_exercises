4b117ae1df92f41b70aabbc1e863f61fc42ffd49
Robinson <robinpaulista85@gmail.com>
2020-12-28 21:11:20 -0300

    Lista de exercícios

--- lista_livro_python/exercicio5.13.cpp
+++ lista_livro_python/exercicio5.13.cpp
@@ -1,3 +1,21 @@
 /* 5.13 Escreva um programa que pergunte o valor inicial de uma dívida e o juro mensal. 
 Pergunte também o valor mensal que será pago. Imprima o número de meses para que a 
 dívida seja paga, o total pago e o total de juros pago. */
+
+#include<stdio.h>
+int main(){
+    float valor_divida, juro_mensal, valor_mensal;
+    printf("Digite o valor inicial da dívida: R$");
+    scanf("%f", &valor_divida);
+    printf("Digite o valor do juro mensal: ");
+    scanf("%f", &juro_mensal);
+    printf("Digite o valor mensal que será pago: R$");
+    scanf("%f", &valor_mensal);
+
+    
+
+
+
+    getchar();
+    return 0;
+}
