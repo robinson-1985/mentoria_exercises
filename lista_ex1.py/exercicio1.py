da8778efb5fc0f1eb21b857739b4ce77a1a65518
Robinson <robinpaulista85@gmail.com>
2021-01-25 19:42:57 -0300

    Lista de exercícios

--- lista_ex1.py/exercicio1.py
+++ lista_ex1.py/exercicio1.py
@@ -1,4 +1,11 @@
 ''' 1. Faça um programa que receba quatro números inteiros, calcule e mostre a soma 
 desses números. '''
 
-soma = input("Digite ")
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+numero3 = int(input("Digite o terceiro número: "))
+numero4 = int(input("Digite o quarto número: "))
+
+soma = numero1 + numero2 + numero3 + numero4
+
+print("A soma dos números é: ", soma)
--- lista_ex1.py/exercicio2.py
+++ lista_ex1.py/exercicio2.py
@@ -1,8 +1,9 @@
 # 2. Faça um programa que receba três notas, calcule e mostre a média aritmética.
 
-n1 = float( input( "Informe a nota 1: ") )
-n2 = float( input( "Informe a nota 2: ") )
-n3 = float( input( "Informe a nota 3: ") )
+nota1 = float( input( "Informe a nota 1: ") )
+nota2 = float( input( "Informe a nota 2: ") )
+nota3 = float( input( "Informe a nota 3: ") )
 
 media = (n1+n2+n3) / 3
-print( "A média é: ", media)
+
+print( "A média aritmética das notas é: ", media)
