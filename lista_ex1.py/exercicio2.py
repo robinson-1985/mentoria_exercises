343ad0bc9e0c1100575af40857d64af6d5b02429
Robinson <robinpaulista85@gmail.com>
2021-01-25 19:59:19 -0300

    Lista de exercícios

--- lista_ex1.py/exercicio2.py
+++ lista_ex1.py/exercicio2.py
@@ -4,6 +4,6 @@ nota1 = float( input( "Informe a nota 1: ") )
 nota2 = float( input( "Informe a nota 2: ") )
 nota3 = float( input( "Informe a nota 3: ") )
 
-media = (n1+n2+n3) / 3
+media = (nota1 + nota2 + nota3) / 3
 
 print( "A média aritmética das notas é: ", media)
--- lista_ex1.py/exercicio3.py
+++ lista_ex1.py/exercicio3.py
@@ -1,2 +1,15 @@
-''' 3.Faça um programa que receba três notas e seus respectivos pesos, calcule e mostre 
+''' 3. Faça um programa que receba três notas e seus respectivos pesos, calcule e mostre 
 a média ponderada.'''
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+peso1 = float(input("Digite o valor do primeiro peso: "))
+peso2 = float(input("Digite o valor do segundo peso: "))
+peso3 = float(input("Digite o valor do terceiro peso: "))
+
+somar = (peso1 + peso2 + peso3)
+
+media_ponderada = (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) / somar
+
+print("A media ponderada das notas é: ", media_ponderada)
