85f99d813e3bca4d73ed6650e62b14419fe25b36
Robinson <robinpaulista85@gmail.com>
2021-01-25 20:16:09 -0300

    Lista de exercícios

--- lista_ex1.py/exercicio3.py
+++ lista_ex1.py/exercicio3.py
@@ -8,7 +8,7 @@ peso1 = float(input("Digite o valor do primeiro peso: "))
 peso2 = float(input("Digite o valor do segundo peso: "))
 peso3 = float(input("Digite o valor do terceiro peso: "))
 
-somar = (peso1 + peso2 + peso3)
+somar = peso1 + peso2 + peso3
 
 media_ponderada = (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) / somar
 
--- lista_ex1.py/exercicio4.py
+++ lista_ex1.py/exercicio4.py
@@ -1,2 +1,9 @@
 ''' 4. Faça um programa que receba o salário de um funcionário, calcule e mostre o novo 
 salário, sabendo-se que este sofreu um aumento de 25%. '''
+
+salario = float(input("Digite o valor do salario: R$ "))
+
+aumento_salario = (salario * 0.25)
+novo_salario = salario + aumento_salario
+
+print("O valor do novo salario será: R$ ", novo_salario)
