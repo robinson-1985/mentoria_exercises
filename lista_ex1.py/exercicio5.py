ed65e61762cd132e856a9f998f6ba146b14c8d3c
Robinson <robinpaulista85@gmail.com>
2021-01-25 22:28:04 -0300

    Lista de exercícios

--- lista_ex1.py/exercicio5.py
+++ lista_ex1.py/exercicio5.py
@@ -1,2 +1,10 @@
 ''' 5.Faça um programa que receba o salário de um funcionário e o percentual de aumento,
 calcule e mostre o valor do aumento e o novo salário. '''
+
+salario = float(input("Digite o valor do salário: R$ "))
+percentual_de_aumento = float(input("Digite a porcentagem de aumento do salario: "))
+
+valor_do_aumento = salario * (percentual_de_aumento / 100)
+novo_salario = salario + valor_do_aumento
+
+print("O novo salário do funcionário é: R$ ", novo_salario)
--- lista_ex1.py/exercicio6.py
+++ lista_ex1.py/exercicio6.py
@@ -1,3 +1,10 @@
-''' 6.Faça um programa que receba o salário base de um funcionário, calcule e mostre o 
+''' 6. Faça um programa que receba o salário base de um funcionário, calcule e mostre o 
 salário a receber, sabendo-se que o funcionário tem gratificação de 5% sobre o salário 
 base e paga imposto de 7% também sobre o salário base. '''
+
+salario_base = float(input("Digite o salario base do funcionário: R$ "))
+gratificacao = salario_base * 0.05
+imposto = salario_base * 0.07
+salario_a_receber = (salario_base + gratificacao) - imposto
+
+print("O salario a receber é: R$ ", salario_a_receber)
