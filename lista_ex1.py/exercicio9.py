30ef93827a2f565f9dffaff6a31306baa30259a6
Robinson <robinpaulista85@gmail.com>
2021-01-27 23:27:59 -0300

    Lista de exercícios

--- lista_ex1.py/exercicio9.py
+++ lista_ex1.py/exercicio9.py
@@ -1,2 +1,9 @@
 ''' 9. Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: 
 Área = (base * altura)/2. '''
+
+base = float(input('Digite o valor da base: '))
+altura = float(input('Digite o valor da altura: '))
+
+area = (base * altura) / 2
+
+print('A área do triângulo é: ', area)
